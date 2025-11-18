d081dd30644759c57a8a411651f0cca6b0fa4878
GotPop <gotpop@users.noreply.github.com>
2025-11-18 18:43:32 +0000

    Deployment test

--- packages/system/src/components/storyblok/BaselineStatusBlock/BaselineStatus.stories.tsx
+++ packages/system/src/components/storyblok/BaselineStatusBlock/BaselineStatus.stories.tsx
@@ -37,7 +37,7 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  name: "Widely Available!!!!",
+  name: "Widely Available????",
   args: {
     data: widelyAvailableFeature,
   },
