cc638cad51f9cd6c953f3e73aa903ddbd2e12cfd
GotPop <gotpop@users.noreply.github.com>
2025-11-20 13:36:45 +0000

    Adding post header stories

--- packages/system/src/components/storyblok/PostHeader/PostHeader.stories.tsx
+++ packages/system/src/components/storyblok/PostHeader/PostHeader.stories.tsx
@@ -1,21 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react"
+import { GridMaster } from "../../ui/GridMaster"
 import { PostHeader } from "./PostHeader"
-import {
-  mockBlogPost,
-  mockCustomStyles,
-  mockEmptyDate,
-  mockEmptyHeading,
-  mockInvalidDate,
-  mockLongHeadingPost,
-  mockNewsPost,
-  mockNoDate,
-  mockNoHeading,
-  mockOldPost,
-  mockRecentPost,
-  mockShortPost,
-  mockTechnicalPost,
-  mockTutorialPost,
-} from "./PostHeader.mocks"
+import { mockBlogPost } from "./PostHeader.mocks"
 
 const meta: Meta<typeof PostHeader> = {
   title: "Storyblok/PostHeader",
@@ -26,90 +12,37 @@ const meta: Meta<typeof PostHeader> = {
       defaultViewport: "desktop",
     },
   },
+  decorators: [
+    (Story) => (
+      <GridMaster>
+        <main>
+          <Story />
+        </main>
+      </GridMaster>
+    ),
+  ],
   argTypes: {
     heading: { control: "text" },
     publishedDate: { control: "text" },
     style: { control: "object" },
   },
   tags: ["autodocs"],
+  args: mockBlogPost,
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const BlogPost: Story = {
-  args: mockBlogPost,
-}
+export const Default: Story = {}
 
-export const TechnicalPost: Story = {
-  args: mockTechnicalPost,
-}
-
-export const NewsPost: Story = {
-  args: mockNewsPost,
-}
-
-export const Tutorial: Story = {
-  args: mockTutorialPost,
-}
-
-export const Short: Story = {
-  args: mockShortPost,
-}
-
-export const LongHeading: Story = {
-  args: mockLongHeadingPost,
-}
-
-export const OldPost: Story = {
-  args: mockOldPost,
-}
-
-export const RecentPost: Story = {
-  args: mockRecentPost,
-}
-
-export const WithCustomStyles: Story = {
-  args: {
-    ...mockBlogPost,
-    style: mockCustomStyles,
+export const Tablet: Story = {
+  globals: {
+    viewport: { value: "tablet" },
   },
 }
 
-export const NoHeading: Story = {
-  args: mockNoHeading,
-}
-
-export const NoDate: Story = {
-  args: mockNoDate,
-}
-
-export const EmptyHeading: Story = {
-  args: mockEmptyHeading,
-}
-
-export const EmptyDate: Story = {
-  args: mockEmptyDate,
-}
-
-export const InvalidDate: Story = {
-  args: mockInvalidDate,
-}
-
-export const TabletView: Story = {
-  args: mockTechnicalPost,
-  parameters: {
-    viewport: {
-      defaultViewport: "tablet",
-    },
-  },
-}
-
-export const MobileView: Story = {
-  args: mockLongHeadingPost,
-  parameters: {
-    viewport: {
-      defaultViewport: "mobile1",
-    },
+export const Mobile: Story = {
+  globals: {
+    viewport: { value: "mobile2" },
   },
 }
